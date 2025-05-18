947f5fdb1f1ffc37a3ace6aa2ee094099053bc96
Mathias Falvo <falvomathias@gmail.com>
2025-05-18 16:49:25 -0300

    * comentarios

--- Backend Avanzado (I)/Unidad 2/desafioDeCodificacion.js
+++ Backend Avanzado (I)/Unidad 2/desafioDeCodificacion.js
@@ -1,10 +1,13 @@
 function duplicarNumeros(numeros){
     
+    //Usamos map para crear un nuevo array con los numeros duplicados
     const numerosDuplicados = numeros.map(numero => numero * 2)
 
+    //Retornamos el array de numeros duplicados
     return numerosDuplicados
 }
 
+//Ejemplo de uso
 const resultado = duplicarNumeros([1,2,3,4,5])
 
 console.log(resultado)
